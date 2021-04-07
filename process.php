baafab259a8e6f756e4ccb968a5f440e2d1fbd68
nadermezher <82063550+nadermezher@users.noreply.github.com>
2021-04-06 19:59:08 -0500

    Update process.php

--- process.php
+++ process.php
@@ -1,13 +1,29 @@
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
 <?php
-
 $UserCode = "A10AE";
 $APIToken = "659630-DGQq6T0GFZsKQHRpk5RBJ4n5QJ2MU2zQ3ZWGInk69myTZ0qFkn";
+$EndpointURL = "https://api.lessannoyingcrm.com";
  
- 
-//The first step is to create a contact
 $Function = "CreateContact";
  
-//Just put the contact info into an array...
+/*
+Notes:
+The only required field is a contact or company name. If you enter a company name
+but not a contact name, we will only create a company and all the other info (phone,
+email, etc.) will be stored with the company. If you enter both a contact AND a company
+name, we'll create both records and all the other info will be stored with the contact.
+*/
+ 
+/*
+When entering a contact, you can either enter a FullName, or five separate fields
+(Salutation, FirstName, MiddleName, LastName, Suffix). If you enter the separate
+names, you are required to have at least a first or a last name (everything else
+is optional). Here are the different formats:
+*/
 $Parameters = array(
     "FullName"=>$_REQUEST['ContactName'],
     "Email"=>array(
@@ -24,48 +40,29 @@ $Parameters = array(
             ),
 );
  
-//...And then use the "CallAPI" function to send the information to LACRM
-$Result = CallAPI($UserCode, $APIToken, $Function, $Parameters);
-//That's it, the contact will now show up in the CRM!
+//The CallAPI function is at the bottom of this file
+CallAPI($EndpointURL, $UserCode, $APIToken, $Function, $Parameters);
  
-//Now let's enter the "Comment" field from the HTML form as a note on the contact's record
-//Get the new ContactId
-$ContactId = $Result['ContactId'];
- 
- 
-$Function = "CreateNote";
-$Parameters = array(
-    "ContactId"=>$ContactId,
-    "Note"=>$_REQUEST['Comment']
-);
- 
-//And pass that note to the API
-$Result = CallAPI($UserCode, $APIToken, $Function, $Parameters);
- 
-/*
-There are all kinds of other things you might want to do here as well such as:
-    -Set a task to follow up with the contact (https://www.lessannoyingcrm.com/help/topic/API_Example_Code_PHP/11/)
-    -Add the contact as a lead (https://www.lessannoyingcrm.com/help/topic/API_Example_Code_PHP/87/)
-    -Add the contact to a group (https://www.lessannoyingcrm.com/help/topic/API_Example_Code_PHP/13/)
-    -Send an email to yourself letting you know a form was submitted (you can use the PHP "mail" function)
-*/
- 
- 
-//Now forward the visitor to an html page confirming that we got their contact info
-header('location:confirm.html');
- 
-  
-/*
-    This function takes all of the settings needed to call the API and puts them into
-    one long URL, and then it makes an HTTP request to that URL.
-*/
-function CallAPI($UserCode, $APIToken, $Function, $Parameters){
-    $APIResult = file_get_contents("https://api.lessannoyingcrm.com?UserCode=$UserCode&APIToken=$APIToken&".
-                "Function=$Function&Parameters=".urlencode(json_encode($Parameters)));
+function CallAPI($EndpointURL, $UserCode, $APIToken, $Function, $Parameters){
+    $PostData = array(
+      'UserCode' => $UserCode,
+      'APIToken' => $APIToken,
+      'Function' => $Function,
+      'Parameters' => json_encode($Parameters),
+    );
+    $Options = array(
+        'http' =>
+            array(
+                'method'  => 'POST', //We are using the POST HTTP method.
+                'header'  => 'Content-type: application/x-www-form-urlencoded',
+                'content' => http_build_query($PostData) // URL-encoded query string.
+            )
+    );
+    $StreamContext  = stream_context_create($Options);
+    $APIResult = file_get_contents("$EndpointURL?UserCode=$UserCode", false, $StreamContext);
     $APIResult = json_decode($APIResult, true);
-      
     if(@$APIResult['Success'] === true){
-        //echo "Success!";
+        echo "Success!";
     }
     else{
         echo "API call failed. Error:".@$APIResult['Error'];
@@ -74,3 +71,5 @@ function CallAPI($UserCode, $APIToken, $Function, $Parameters){
     return $APIResult;
 }
 ?>
+</body>
+</html>
